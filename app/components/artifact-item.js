802918b294ef9dbf46382100f5cee44af9a12612
Amy Truong <avytrv@gmail.com>
2023-09-10 16:00:30 -0400

    Edit artifact labels and formatting

--- app/components/artifact-item.js
+++ app/components/artifact-item.js
@@ -1,29 +1,29 @@
 import { Card, Text } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
-import setLabels from '../../data/set-labels.json';
+import labels from '../../data/labels.json';
 import subStats from '../../data/substats.json';
 
-export default function ArtifactItem({ data }) {
-    return (
-        <Card style={styles.card}>
-            <Text>{ setLabels[data.set] }</Text>
-            <Text>{ data.type }</Text>
-            <Text>{ data.mainStatName } : { data.mainStatValue }</Text>
-            {subStats.map((subStat) => {
-                if (parseFloat(data[subStat.value]) !== 0) {
-                    return (
-                        <Text>{ subStat.label } : {data[subStat.value]}</Text>
-                    )
-                }
-            })
-            }
-        </Card>
-    );
+function ArtifactItem({ data }) {
+  return (
+    <Card style={styles.card}>
+      <Text variant='titleLarge'>{ labels[data.set] } | { labels[data.type] }</Text>
+      <Text variant='titleMedium'>{ labels[data.mainStatName] } : { data.mainStatValue }</Text>
+      {subStats.map((subStat) => {
+        if (parseFloat(data[subStat.value]) !== 0) {
+          return (
+            <Text>{ subStat.label } : {data[subStat.value]}</Text>
+            )
+          }
+        })}
+      </Card>
+  );
 }
-
+    
 const styles = StyleSheet.create({
-    card: {
-      padding: 16,
-      margin: 16,
-    },
-  });
+  card: {
+    padding: 16,
+    margin: 16,
+  },
+});
+
+export default ArtifactItem;

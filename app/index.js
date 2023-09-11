2f49887d0bc4566e2716fb459f89d6f670d66d9a
madhura112 <madhura25.16@gmail.com>
2023-09-11 17:47:40 -0400

    fix initial state of artifacts screen

--- app/index.js
+++ app/index.js
@@ -1,88 +1,103 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { StyleSheet, View, TextInput, Button, Alert, Text } from 'react-native';
 import { auth, signInWithEmailAndPassword, ref, database, get } from '../firebase.js';
-import { Appbar, FAB } from 'react-native-paper';
+import { FAB } from 'react-native-paper';
 import { Link } from 'expo-router';
-import testArtifact from '../data/test-artifact.json';
+// import testArtifact from '../data/test-artifact.json';
 import ArtifactItem from './components/artifact-item.js';
 
 const Login = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [loggedIn, setLoggedIn] = useState(false);  // New state for login status
-  const [artifacts, setArtifacts] = useState([testArtifact]); 
+  const [loggedIn, setLoggedIn] = useState(false);
+  // const [artifacts, setArtifacts] = useState([testArtifact]); 
 
   const currentUserUIDRef = useRef(null);
   const currentSnapshot = useRef(null);
   const artifactsIds = useRef(null);
   const currentUserArtifactData = useRef(null);
 
+  const [loadingArtifacts, setLoadingArtifacts] = useState(false);
+
+
+  useEffect(() => {
+    if (loggedIn) {
+      setLoadingArtifacts(true);
+      
+      const dbRef = ref(database);
+      get(dbRef, `users/${currentUserUIDRef.current}/users`).then((snapshot) => {
+        if (snapshot.exists()) {
+          currentSnapshot.current = snapshot;
+  
+          const deepCopy = JSON.parse(JSON.stringify(currentSnapshot.current));
+          artifactsIds.current = Object.keys(deepCopy?.users?.[currentUserUIDRef.current]?.["ownedItems"]);
+  
+          currentUserArtifactData.current = [];
+          for (var i = 0; i < artifactsIds.current.length; i++) {
+            currentUserArtifactData.current.push(deepCopy?.items?.[artifactsIds.current[i]]);
+          }
+        } else {
+          console.log("No data found for current user.");
+        }
+        
+        setLoadingArtifacts(false);
+      });
+    }
+  }, [loggedIn]);
   const loginUser = async () => {
     try {
       const userCredential = await signInWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
       currentUserUIDRef.current = user.uid;
       console.log("Successfully logged in!", currentUserUIDRef.current);
-      setLoggedIn(true);  // Set the loggedIn state to true on successful login
+      setLoggedIn(true);
     } catch (error) {
         console.log("Error logging in. Please try again.")
         Alert.alert("Error logging in:", error.message);
     }
   }; 
 
-  // If logged in, redirect to the desired screen
   if (loggedIn) {
-    const dbRef = ref(database);
-    get(dbRef, `users/${currentUserUIDRef.current}/users`).then((snapshot) => {
-      if (snapshot.exists()) {
-        currentSnapshot.current = snapshot;
-        
-        const deepCopy = JSON.parse(JSON.stringify(currentSnapshot.current));
-        artifactsIds.current = Object.keys(deepCopy?.users?.[currentUserUIDRef.current]?.["ownedItems"]);
-        
-        currentUserArtifactData.current = [];
-        for (var i = 0; i < artifactsIds.current.length; i++) {
-          currentUserArtifactData.current.push(deepCopy?.items?.[artifactsIds.current[i]]);
-        }
-      } else {
-        console.log("No data found for current user.");
-      }
-    });
-
-    if (currentUserArtifactData.current == null || currentUserArtifactData.current.length == 0) {
+    if (loadingArtifacts) {
       return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Loading...</Text>
+        </View>
+      );
+    } else if (currentUserArtifactData.current == null || currentUserArtifactData.current.length == 0) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{fontWeight: 'bold'}}></Text>
+            <Text>Click the '+' sign to add your artifact info!</Text>
+            <Link href='/add' asChild>
+              <FAB
+                icon='plus'
+                style={styles.fab}
+              />
+            </Link>
+        </View>
+      )
+  } else {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
         <Text style={{fontWeight: 'bold'}}></Text>
-        <Text>Click the '+' sign to add your artifact info!</Text>
+        {currentUserArtifactData.current ? 
+            currentUserArtifactData.current.map((artifact, index) => (
+                <ArtifactItem key={index} data={artifact} />
+            ))
+        : 
+            <Text>Loading...</Text>  // This is a placeholder; you can replace it with a loading spinner or other content
+        }
         <Link href='/add' asChild>
-          <FAB
-            icon='plus'
-            style={styles.fab}
-          />
+            <FAB
+                icon='plus'
+                style={styles.fab}
+            />
         </Link>
     </View>
-      )
-    } else {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-          <Text style={{fontWeight: 'bold'}}></Text>
-          {currentUserArtifactData.current ? 
-              currentUserArtifactData.current.map((artifact, index) => (
-                  <ArtifactItem key={index} data={artifact} />
-              ))
-          : 
-              <Text>Loading...</Text>  // This is a placeholder; you can replace it with a loading spinner or other content
-          }
-          <Link href='/add' asChild>
-              <FAB
-                  icon='plus'
-                  style={styles.fab}
-              />
-          </Link>
-      </View>
-      );
-    }
+    );
   }
+}
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', paddingHorizontal: 16 }}>
